4fe6a27a98be15e6d5c21bea79d01eb4765b26f0
wangchen11 <1012371864@qq.com>
2020-01-09 12:10:19 +0800

    修复龙骨导出的Spine 3.3数据加载闪退的问题。

--- cocos2d/cocos/editor-support/spine/SkeletonJson.c
+++ cocos2d/cocos/editor-support/spine/SkeletonJson.c
@@ -593,8 +593,8 @@ spSkeletonData* spSkeletonJson_readSkeletonData (spSkeletonJson* self, const cha
 
 	skeleton = Json_getItem(root, "skeleton");
 	if (skeleton) {
-		MALLOC_STR(skeletonData->hash, Json_getString(skeleton, "hash", 0));
-		MALLOC_STR(skeletonData->version,  Json_getString(skeleton, "spine", 0));
+		MALLOC_STR(skeletonData->hash, Json_getString(skeleton, "hash", "0"));
+		MALLOC_STR(skeletonData->version,  Json_getString(skeleton, "spine", "Dev"));
 		skeletonData->width = Json_getFloat(skeleton, "width", 0);
 		skeletonData->height = Json_getFloat(skeleton, "height", 0);
 	}
